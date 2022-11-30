6d1e5787c6522b32fae8ff21333ac62e1d981d44
Raju-Challagundla <rchalla5@uncc.edu>
2022-11-30 10:17:32 -0500

    Fixing classpath issue

--- fastfeast_dump_current.sql
+++ fastfeast_dump_current.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `fastfeast_fall_2022` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `fastfeast_fall_2022`;
 -- MySQL dump 10.13  Distrib 8.0.26, for macos11 (x86_64)
 --
 -- Host: localhost    Database: dumptest_2
